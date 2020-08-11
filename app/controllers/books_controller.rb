8be164f63dc55a863c81a069f669ed856728e1d6
vagrant <sotahashimoto316@gmail.com>
2020-08-11 11:03:36 +0000

    errorFix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,13 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to books_path
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   private
